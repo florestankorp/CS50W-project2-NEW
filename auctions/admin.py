4dd7fe8e18545970fff8e625ddedcf8fa5647edc
Florestan Korp <florestankorp@gmail.com>
2020-07-12 22:07:50 +0200

    started work on models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Bid, Comment, Listing, User, Watchlist
+
 # Register your models here.
+admin.site.register(Bid)
+# admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Listing)
+admin.site.register(User)
+admin.site.register(Watchlist)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,37 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=64)
+
+
+class Listing(models.Model):
+    CATEGORIES = (("LAP", "Laptop"), ("CON", "Console"), ("GAD", "Gadget"), ("GAM", "Game"), ("TEL", "TV"))
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    starting_bid = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    image_url = models.URLField(max_length=200)
+    category = models.CharField(max_length=8, choices=CATEGORIES)
+    active = models.BooleanField()
+
+    def __str__(self):
+        return f"title: {self.title}"
+
+
+class Bid(models.Model):
+    amount = models.PositiveIntegerField()
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=256)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ManyToManyField(Listing, blank=True)
